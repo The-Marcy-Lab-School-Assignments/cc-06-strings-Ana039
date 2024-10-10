09814e1e5f8155e9e04695197ace6d1bef1c05ac
Anayely <anayelialmonte@gmail.com>
2024-10-10 11:01:57 -0400

    up to questoion 2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //write your code here
+const reverseString = (str) => {
+let s = ""
+for (let i = str.length - 1; i >= 0; i--) {
+    s += str[i]
+}
+return s;
+}
+console.log(reverseString("hello"));
+console.log(reverseString("oo"));
+console.log(reverseString(""));
+
